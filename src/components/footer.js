e62250a87c036764ebb46c95be9c5422bd64fad4
Ryan Jackson <rcjackson96@gmail.com>
2023-03-21 16:15:53 -0500

    Changes to CSS to add fonts

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,22 +1,10 @@
-const Footer = props => {
+function Footer() {
   return (
-    <footer className="mt-5">
-      <div className="d-flex justify-content-center mt-3 pt-3 pb-1">
-        <a href="https://github.com/JimothyThunderguns" target="_blank" rel="noreferrer">
-          <i className="bi bi-github m-3 fs-2"></i>
-        </a>
-        <a href="https://www.linkedin.com/in/ryan-jackson-9a419a250/" target="_blank" rel="noreferrer">
-          <i className="bi bi-linkedin m-3 fs-2"></i>
-        </a>
-        <a href="mailto:rcjackson96@gmail.com">
-          <i className="bi bi-envelope-at-fill m-3 fs-2"></i>
-        </a>
-        <a href="tel:17153474664">
-          <i className="bi bi-telephone-fill m-3 fs-2"></i>
-        </a>
-      </div>
-      <p className="ms-3">&copy; Ryan Jackson 2023</p>
-    </footer>
+      <footer className="flex-row px-1">
+          <a href="https://github.com/jonpfluger"><i class="bi bi-github"></i></a>
+          <a href="https://www.linkedin.com/in/jonathanjeffreypfluger/"><i class="bi bi-linkedin"></i></a>
+          <a href="https://stackoverflow.com/users/19670475/jon-pfluger"><i class="bi bi-stack-overflow"></i></a>
+      </footer>
   )
 }
 
