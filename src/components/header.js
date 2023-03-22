b60901f9366073efac1d4266aefc54291ce2a5ae
Ryan Jackson <rcjackson96@gmail.com>
2023-03-22 15:55:06 -0500

    useless changes

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,10 @@
-import Navbar from "./Navbar"
-
-const Header = ({ view, setView }) => {
-  return (
-    <nav className="navbar navbar-expand-lg p-4">
-      <div className="container mx-auto">
-        <h1 className="name">Ryan Jackson</h1>
-        <Navbar
-          view={view}
-          setView={setView}
-        />
-      </div>
-    </nav>
-  )
+const Header = props => {
+    return (
+        <header>
+            <h1>Ryan Jackson</h1>
+            {props.children}
+        </header>
+    )
 }
 
 export default Header
