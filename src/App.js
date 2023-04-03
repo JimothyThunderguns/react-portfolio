49b9ac872ad157ed08fffc1361c5a932f2cb8875
Ryan Jackson <rcjackson96@gmail.com>
2023-04-03 12:53:29 -0500

    rework of app.js and changing home.jsx to About.js

--- src/App.js
+++ src/App.js
@@ -4,38 +4,8 @@ import './App.css';
 import './components/Header';
 import './components/Footer';
 
-
-//function
-function App() {
-  const [view, setView] = useState('home')
-
-  const renderView = () => {
-    if (view === 'home') {
-      return <Home />
-    } else if (view === 'portfolio') {
-      return <Portfolio />
-    } else if (view === 'contact') {
-      return <Contact />
-    } else if (view === 'resume') {
-      return <Resume />
-    } else {
-      return 'No matching view found'
-    }
-  }
-
-
-  return (
-  <>
-    <header 
-      view={view}
-      setView={setView}
-    />
-    { renderView() }
-    <footer/>
-
-  </>
-);
-}
-
-
-export default App;
+//pages
+import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
+import Contact from './pages/Contact';
