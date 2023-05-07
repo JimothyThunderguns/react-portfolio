4f3639185a41aedb5f52f4c587e01a5bc9eba432
Ryan Jackson <rcjackson96@gmail.com>
2023-05-07 11:51:39 -0500

    hahaaaaaaaa

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 //components
 import { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import './components/Header';
 import './components/Footer';
 
 //pages
-import Home from './pages/Home';
+import AboutMe from './pages/About';
 import Portfolio from './pages/Portfolio';
 import Resume from './pages/Resume';
 import Contact from './pages/Contact';
+
+function App() {
+  const [view, setView] = useState{}
+   const renderView = () => {
+
+   }
+}
+
+export default App;
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,1 +1,19 @@
 import React from 'react'
+import { BsGithub } from 'react-icons/bs'
+
+const Project = ({ project }) => {
+    const { title, description, githubUrl, liveUrl, techs, image } = project
+    return (
+        <div className="card" style={{ width: '18rem' }}>
+            <img src={image} className="card-img-top" alt={title} />
+            <div className="card-body">
+                <h3 className="card-title">{title}</h3>
+                <p className="card-text">{description}</p>
+                <p className="card-techs">{techs}</p>
+                <a href={githubUrl} className="btn"><BsGithub />   Project Repo</a>
+            </div>
+        </div >
+    )
+}
+
+export default Project;
